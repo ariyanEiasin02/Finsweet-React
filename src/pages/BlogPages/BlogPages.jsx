2d20bb67e4377112c2cdbf424b17cc5db4b0ef56
ariyanEiasin02 <ariyaneiasin56@gmail.com>
2025-11-11 12:36:38 +0600

    Refactor App and BlogPages components, update OurBlog to use BlogCard, and modify blogData with new entries and images

--- src/pages/BlogPages/BlogPages.jsx
+++ src/pages/BlogPages/BlogPages.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import Environment from '../../components/Environment/Environment'
 import OurBlog from '../../components/OurBlog/OurBlog'
 
 const BlogPages = () => {
   return (
     <div className='pt-20'>
-      <Environment />
+      {/* <Environment /> */}
       <OurBlog />
     </div>
   )
